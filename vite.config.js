5e66a83a984842d4067ec06e6464595f44e81807
pinti0001 <pinti000pk@gmail.com>
2026-05-24 19:23:23 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://internshala.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            proxyReq.setHeader('Origin', 'https://internshala.com');
+            proxyReq.setHeader('Referer', 'https://internshala.com/');
+          });
+        },
+      },
+    },
+  },
 });
